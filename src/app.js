fa7f4177d5b7566aaa47ffbdc25546ec67568ec4
Thyago Dias <thyago.dias@unemat.br>
2020-04-09 21:05:24 -0400

    adiciona rotas de listagem, criação e edição de repositórios

--- src/app.js
+++ src/app.js
@@ -1,33 +1,75 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
+/**
+ * Rota que lista todos os repositórios
+ */
+app.get('/repositories', (request, response) => {
+  return response.json(repositories)
+})
+
+/**
+ * A rota deve receber title, url e techs dentro do corpo da requisição, sendo a URL o link para o github desse repositório.
+ * Ao cadastrar um novo projeto, ele deve ser armazenado dentro de um objeto no seguinte formato:
+ * { id: "uuid", title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 };
+ * Certifique-se que o ID seja um UUID, e de sempre iniciar os likes como 0.
+ */
+app.post('/repositories', (request, response) => {
+  const data = request.body
+
+  const repository = {
+    id: uuid(),
+    title: data.title,
+    url: data.url,
+    techs: data.techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.status(201).json(repository)
+})
+
+/**
+ * A rota deve alterar apenas o título, a url e as techs do repositório que possua o
+ * id igual ao id presente nos parâmetros da rota;
+ */
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex(
+    repository => repository.id === id
+  )
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repositório não encontrado' })
+  }
+
+  const { likes } = repositories[repositoryIndex]
+
+  const repository = { id, title, url, techs, likes }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json({ repository })
+})
+
+app.delete('/repositories/:id', (req, res) => {
   // TODO
-});
+})
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
-});
+})
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
-
-module.exports = app;
+module.exports = app
